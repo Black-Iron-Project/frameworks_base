605a3c5ee66e845c61bf580ccaeb39d374392ea0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 22:45:50 +0000

    SystemUI: Add Sync tile
    Also includes squashed change:
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Wed Jun 14 11:53:05 2023 +0300
    
        fixup! SystemUI: Add Sync tile
    
        Change-Id: I08460b33f23b57966f9d50bea9e2b88afe89f98c
    
    Change-Id: I0c0bb51e736a01661566f1118f955e32eba96063

--- packages/SystemUI/src/com/android/systemui/blackiron/BlackironModule.kt
+++ packages/SystemUI/src/com/android/systemui/blackiron/BlackironModule.kt
@@ -8,6 +8,7 @@ package com.android.systemui.blackiron
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.SyncTile
 
 import dagger.Binds
 import dagger.Module
@@ -27,4 +28,10 @@ interface BlackironModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject SyncTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SyncTile.TILE_SPEC)
+    fun bindSyncTile(syncTile: SyncTile): QSTileImpl<*>
 }
