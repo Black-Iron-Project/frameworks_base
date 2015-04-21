997771c430e5455e2e20875785ea10ccd71f6876
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Change-Id: I367dbbf7977712d8ad085866387730f84074bf68
    
    Change-Id: I6ab1d07d63f428f1fe60aee629894b549c1ae849
    
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- packages/SystemUI/src/com/android/systemui/blackiron/BlackironModule.kt
+++ packages/SystemUI/src/com/android/systemui/blackiron/BlackironModule.kt
@@ -7,6 +7,7 @@ package com.android.systemui.blackiron
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 
 import dagger.Binds
 import dagger.Module
@@ -20,4 +21,10 @@ interface BlackironModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 }
