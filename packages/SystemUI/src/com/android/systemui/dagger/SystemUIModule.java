214452205933816afc4e9c17fff87e31bfc41c17
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Thu Feb 4 11:10:22 2016 -0800
    
        SystemUI: Add caffeine qs tile
    
        Change-Id: I5d4aa97ae63fc0bcc40f0fa7d9305efbba091029
    
    Author: chillger <alexander.loskutov@gmail.com>
    Date:   Fri May 22 13:16:00 2020 +0200
    
        SystemUI: Long press Caffeine for infinite duration
    
        Change-Id: Ic49539dcda6d7d6b1dab362eb8ec168d2ec65b07
    
    Change-Id: I16eab8d0ec496bbac7e32eea370c846573349cc9
    
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -47,6 +47,7 @@ import com.android.systemui.controls.dagger.ControlsModule;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.SystemUser;
 import com.android.systemui.demomode.dagger.DemoModeModule;
+import com.android.systemui.blackiron.BlackironModule;
 import com.android.systemui.display.DisplayModule;
 import com.android.systemui.doze.dagger.DozeComponent;
 import com.android.systemui.dreams.dagger.DreamModule;
@@ -172,6 +173,7 @@ import javax.inject.Named;
             DreamModule.class,
             ControlsModule.class,
             DemoModeModule.class,
+            BlackironModule.class,
             DisableFlagsModule.class,
             FalsingModule.class,
             FlagsModule.class,
