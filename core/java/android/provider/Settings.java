84f347bde6df8e4a5c88a0cf05d15b87e0eb8004
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2019-11-30 13:41:05 +0100
        SystemUI: Bring back good ol' circle battery style
    
        * This code is based on the old Java implementation and converted to Kotlin
    
        * Instead of creating different style within the same drawable
          create one on its own, this makes the code more readable and future proof
    
        Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2021-04-08 21:41:24 +0100
        SystemUI: Fix status bar battery icon title
    
        The wrong string was being picked up which is not capitalized
        and not even localized. Use the already localized string
        from SystemUI package.
    
        Change-Id: I4be427c95a0fb98c77d450e0791bb8e3442ed3a1
    
    Change-Id: I025811006cf6088d94b6a2e2e5520543dedd7f98

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5810,6 +5810,26 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
+                "status_bar_show_battery_percent";
+
+       /**
          * These are all public system settings
          *
          * @hide
@@ -5932,6 +5952,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING);
             PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
+            PRIVATE_SETTINGS.add(STATUS_BAR_BATTERY_STYLE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_SHOW_BATTERY_PERCENT);
         }
 
         /**
