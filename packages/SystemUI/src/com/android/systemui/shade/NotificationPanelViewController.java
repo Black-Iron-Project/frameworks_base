0d86232f862c676910f0ae487f6775bf415a55e9
maxwen <max.weninger@gmail.com>
2020-02-11 13:31:04 +0100

    base: cleanup notification pulse values
    a few unneded and a few with wrong names
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1680,7 +1680,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
         if (!hasVisibleNotifications) {
             Settings.System.putIntForUser(mView.getContext().getContentResolver(),
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0,
+                    Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0,
                     UserHandle.USER_CURRENT);
         }
         mClockPositionAlgorithm.setup(
@@ -3227,7 +3227,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private void updatePulseLightState(boolean dozing) {
         boolean mAmbientLights = Settings.System.getIntForUser(
-                mView.getContext().getContentResolver(), Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
+                mView.getContext().getContentResolver(), Settings.System.AOD_NOTIFICATION_PULSE,
                 0, UserHandle.USER_CURRENT) != 0;
         if (DEBUG_PULSE_LIGHT) {
             Log.d(TAG, "updatePulseLightState dozing = " + dozing + " mAmbientLights = "  + mAmbientLights);
@@ -3243,7 +3243,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 // screen on!
                 mPulseLightsView.setVisibility(View.GONE);
                 Settings.System.putIntForUser(mView.getContext().getContentResolver(),
-                        Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0,
+                        Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0,
                         UserHandle.USER_CURRENT);
                 mPulseLightHandled = true;
             }
@@ -3257,13 +3257,13 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 animatePulse =
                 !mDozeParameters.getDisplayNeedsBlanking() && mDozeParameters.getAlwaysOn();
         boolean pulseLights = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
-                Settings.System.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+                Settings.System.NOTIFICATION_PULSE, 0, UserHandle.USER_CURRENT) != 0;
         boolean ambientLights = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+                Settings.System.AOD_NOTIFICATION_PULSE, 0, UserHandle.USER_CURRENT) != 0;
         boolean activeNotif = mNotificationStackScrollLayoutController.hasActiveClearableNotifications(ROWS_ALL);
         int pulseReason = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
                 Settings.System.PULSE_TRIGGER_REASON, DozeLog.PULSE_REASON_NONE, UserHandle.USER_CURRENT);
-        boolean pulseReasonNotification = pulseReason == DozeLog.PULSE_REASON_NOTIFICATION;
+        boolean pulseReasonNotification = pulseReason == DozeLog.AOD_NOTIFICATION_PULSE_CLEAR;
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -3295,7 +3295,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                         // tell power manager that we want to enable aod
                         // must do that here already not on pulsing = false
                         Settings.System.putIntForUser(mView.getContext().getContentResolver(),
-                                Settings.System.AMBIENT_NOTIFICATION_LIGHT, 1,
+                                Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 1,
                                 UserHandle.USER_CURRENT);
                     }
                 }
@@ -3309,7 +3309,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                     // no active notifications or just pulse without aod - so no reason to continue
                     mPulseLightsView.setVisibility(View.GONE);
                     Settings.System.putIntForUser(mView.getContext().getContentResolver(),
-                            Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0,
+                            Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0,
                             UserHandle.USER_CURRENT);
                 }
             }
