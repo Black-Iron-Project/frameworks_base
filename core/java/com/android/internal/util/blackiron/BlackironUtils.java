85195da0ee1cc37bdf2b0703ea45ef9ab6fddf1e
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 09:14:37 +0530

    Utils: add method to get today's passed time
    Change-Id: I521a412fff810b0ab098b2a30431358314a11d6b
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- core/java/com/android/internal/util/blackiron/BlackironUtils.java
+++ core/java/com/android/internal/util/blackiron/BlackironUtils.java
@@ -34,6 +34,7 @@ import android.os.Looper;
 import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.os.SystemClock;
+import android.text.format.Time;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.InputDevice;
@@ -117,6 +118,15 @@ public class BlackironUtils {
 
         NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifi.isConnected();
+    }
+
+	// Returns today's passed time in Millisecond
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 
 	// Check if device is connected to the internet
