8862bdddc048fc265d4d2c8a7bfc2f7660f7b402
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 13:16:24 +0000

    Fix QS clock being white on light theme
    Steps to reproduce without this commit:
    - Turn off dark theme
    - Play a song and wait for media player to show in QS
    - Confirm clock text color is white
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
@@ -18,10 +18,8 @@ package com.android.systemui.qs;
 
 import android.os.Bundle;
 
-import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.battery.BatteryMeterViewController;
-import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.demomode.DemoMode;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.flags.FeatureFlags;
@@ -66,7 +64,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
 
     private boolean mListening;
 
-    private SysuiColorExtractor mColorExtractor;
     private ColorExtractor.OnColorsChangedListener mOnColorsChangedListener;
 
     @Inject
@@ -76,7 +73,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
             DemoModeController demoModeController,
             QuickQSPanelController quickQSPanelController,
             QSCarrierGroupController.Builder qsCarrierGroupControllerBuilder,
-            SysuiColorExtractor colorExtractor,
             QSExpansionPathInterpolator qsExpansionPathInterpolator,
             FeatureFlags featureFlags,
             VariableDateViewController.Factory variableDateViewControllerFactory,
@@ -107,7 +103,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
 
         mIconManager = tintedIconManagerFactory.create(mIconContainer, StatusBarLocation.QS);
         mDemoModeReceiver = new ClockDemoModeReceiver(mClockView);
-        mColorExtractor = colorExtractor;
         mOnColorsChangedListener = (extractor, which) -> {
             final boolean lightTheme = mColorExtractor.getNeutralColors().supportsDarkText();
             mClockView.onColorsChanged(lightTheme);
