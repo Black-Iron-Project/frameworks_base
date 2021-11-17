3bb93d7b28a20a9840dfb1759394ec66949590ac
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-17 14:42:28 +0200

    QuickStatusBarHeader: Add date & battery click actions
    Adapted to A14 (bring back what was done in/after merges before)
    Clock already had an action programmed in A14 so just follow that code style
    
    Open default clock & calendar accordingly
    
    Change-Id: I2326f942ddb22b572c9d4db1db60d4faa9e13a08

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -25,10 +25,13 @@ import android.content.Intent
 import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.os.Trace
 import android.os.Trace.TRACE_TAG_APP
 import android.provider.AlarmClock
+import android.provider.CalendarContract
 import android.util.Pair
 import android.view.DisplayCutout
 import android.view.View
@@ -332,6 +335,8 @@ constructor(
             v.pivotY = v.height.toFloat() / 2
         }
         clock.setOnClickListener { launchClockActivity() }
+        date.setOnClickListener { launchDateActivity() }
+        batteryIcon.setOnClickListener { launchBatteryActivity() }
 
         dumpManager.registerDumpable(this)
         configurationController.addCallback(configurationControllerListener)
@@ -379,6 +384,19 @@ constructor(
         } else {
             activityStarter.postStartActivityDismissingKeyguard(DEFAULT_CLOCK_INTENT, 0 /*delay */)
         }
+    }
+
+    internal fun launchDateActivity() {
+        val builder: Uri.Builder = CalendarContract.CONTENT_URI.buildUpon()
+        builder.appendPath("time")
+        builder.appendPath(System.currentTimeMillis().toString())
+        val todayIntent: Intent = Intent(Intent.ACTION_VIEW, builder.build())
+        activityStarter.postStartActivityDismissingKeyguard(todayIntent, 0 /*delay */)
+    }
+
+    internal fun launchBatteryActivity() {
+        activityStarter.postStartActivityDismissingKeyguard(Intent(
+                Intent.ACTION_POWER_USAGE_SUMMARY), 0)
     }
 
     private fun loadConstraints() {
