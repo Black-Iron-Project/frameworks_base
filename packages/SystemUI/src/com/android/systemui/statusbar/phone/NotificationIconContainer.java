4c2d58697d872f527ec442c4b15f2dc7f56b6ee5
spkal01 <spkal01@users.noreply.github.com>
2021-12-09 18:32:21 +0000

    SystemUI: Make app icons in statusbar optional [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -25,6 +25,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Icon;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.MathUtils;
 import android.util.Property;
@@ -907,6 +909,8 @@ public class NotificationIconContainer extends ViewGroup {
                     }
                 }
                 icon.setVisibleState(visibleState, animationsAllowed);
+                boolean newIconStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                            Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
                 if (icon.getStatusBarIcon().pkg.contains("systemui"))
                 icon.setIconColor(mOverrideIconColor ? mThemedTextColorPrimary : iconColor,
                         needsCannedAnimation && animationsAllowed);
@@ -951,7 +955,9 @@ public class NotificationIconContainer extends ViewGroup {
             super.initFrom(view);
             if (view instanceof StatusBarIconView) {
                 StatusBarIconView icon = (StatusBarIconView) view;
-                if (icon.getStatusBarIcon().pkg.contains("systemui"))
+                boolean newIconStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                            Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !newIconStyle)
                     iconColor = ((StatusBarIconView) view).getStaticDrawableColor();
                 else
                     return;
