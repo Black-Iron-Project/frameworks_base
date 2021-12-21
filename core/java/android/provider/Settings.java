417a0ebcc9ea7fa53d5be8efdcaa1829c65fcabf
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    Bring back UDFPS animation
    neobuddy89: Adapted for Android 12L
    Stallix - Adapted for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: 00day0 <00day0@users.noreply.github.com>
    
    [Dhina17]
    - Drop Udfps icon customization
    - Use ContentObserver instead of TunerService
    
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id: I0f0fcde998ad3d80f22f76f7136f141331ca6c7c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6870,6 +6870,16 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM = "udfps_anim";
+
+        /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
+
+        /**
          * Three Finger Gesture from Oppo
          * @hide
          */
--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -37,6 +37,7 @@ import android.content.IntentFilter;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.biometrics.BiometricFingerprintConstants;
+import android.hardware.biometrics.BiometricOverlayConstants;
 import android.hardware.biometrics.SensorProperties;
 import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
@@ -229,6 +230,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private BoostFramework mPerf = null;
     private boolean mIsPerfLockAcquired = false;
     private static final int BOOST_DURATION_TIMEOUT = 2000;
+
+    private UdfpsAnimation mUdfpsAnimation;
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -907,13 +910,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mLatencyTracker = latencyTracker;
         mActivityLaunchAnimator = activityLaunchAnimator;
         mAlternateTouchProvider = alternateTouchProvider.map(Provider::get).orElse(null);
-        mSensorProps = new FingerprintSensorPropertiesInternal(
-                -1 /* sensorId */,
-                SensorProperties.STRENGTH_CONVENIENCE,
-                0 /* maxEnrollmentsPerUser */,
-                new ArrayList<>() /* componentInfo */,
-                FingerprintSensorProperties.TYPE_UNKNOWN,
-                false /* resetLockoutRequiresHardwareAuthToken */);
+        mSensorProps = findFirstUdfps();
 
         mBiometricExecutor = biometricsExecutor;
         mPrimaryBouncerInteractor = primaryBouncerInteractor;
@@ -956,6 +953,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mPerf = new BoostFramework();
 
         mUdfpsVendorCode = mContext.getResources().getInteger(com.android.systemui.R.integer.config_udfps_vendor_code);
+
+        if (com.android.internal.util.blackiron.BlackironUtils.isPackageInstalled(mContext,
+                "com.blackiron.udfps.resources")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
 
     /**
@@ -985,6 +987,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         }
     }
 
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void dozeTimeTick() {
         if (mOverlay != null) {
@@ -1008,6 +1021,12 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason ==
+                    BiometricOverlayConstants.REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -1277,6 +1296,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
     }
 
     private void onFingerUp(long requestId, @NonNull UdfpsView view) {
@@ -1330,6 +1352,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerUp();
             }
+        }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
         }
         mOnFingerDown = false;
         if (isOptical()) {
