e14335fa569bcee3828ec94ea9083bc6a4e8b617
cjh1249131356 <cjh1249131356@gmail.com>
2022-09-25 14:50:31 +0800

    base: Add mock oplus LinearmotorVibratorService
    * To get haptic feedback in OnePlus / Oplus Camera.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [@neobuddy89: Remove unnecessary bits.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -228,6 +228,7 @@ import com.android.server.twilight.TwilightService;
 import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.UsageStatsService;
 import com.android.server.utils.TimingsTraceAndSlog;
+import com.android.server.vibrator.LinearmotorVibratorService;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.vr.VrManagerService;
 import com.android.server.wearable.WearableSensingManagerService;
@@ -2695,6 +2696,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartLinearmotorVibratorService");
+            mSystemServiceManager.startService(LinearmotorVibratorService.class);
             t.traceEnd();
         }
 
