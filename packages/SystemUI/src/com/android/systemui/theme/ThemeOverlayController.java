0734e9b8353c546b475e60cf0627634de61dd7ec
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-21 18:30:02 +0530

    SystemUI: Reload theme only if black theme is enabled
    Currently it force reloads theme on UI mode change always
    for black theme to set correctly, But this has the side
    effect of janky transition between dark<->light mode and
    other glitches in QS. Perform this only if we have black
    theme actually enabled.
    
    Change-Id: I372bdb51dacc54305d47cd42a5b165ac35609bd8

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -969,9 +969,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
             categoryToPackage.put(OVERLAY_CATEGORY_DYNAMIC_COLOR, mDynamicOverlay.getIdentifier());
         }
 
-        boolean isBlackMode = (Settings.Secure.getIntForUser(
-                mContext.getContentResolver(), Settings.Secure.BERRY_BLACK_THEME,
-                0, currentUser) == 1) && isNightMode();
+        boolean isBlackMode = isBlackThemeEnabled() && isNightMode();
         if (categoryToPackage.containsKey(OVERLAY_CATEGORY_SYSTEM_PALETTE) && isBlackMode) {
             OverlayIdentifier blackTheme = new OverlayIdentifier(OVERLAY_BERRY_BLACK_THEME);
             categoryToPackage.put(OVERLAY_CATEGORY_SYSTEM_PALETTE, blackTheme);
@@ -1026,6 +1024,11 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
             }
         }
         return style;
+    }
+
+    private boolean isBlackThemeEnabled() {
+        return Settings.Secure.getIntForUser(
+            mContext.getContentResolver(), Settings.Secure.BERRY_BLACK_THEME, 0, mUserTracker.getUserId()) == 1;
     }
 
     @Override
