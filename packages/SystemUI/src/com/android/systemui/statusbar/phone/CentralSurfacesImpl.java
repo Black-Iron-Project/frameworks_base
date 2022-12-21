fefc315a0c0591aa7769c9160a265b54bd97367a
Christian Oder <myself5@carbonrom.org>
2022-12-22 00:14:09 +0100

    fwb: Restore mMainHandler in CentralSurfacesImpl
    This has been removed in c094cbe6bbe49bc0629212119c0c4e56db8a70bf for being unecessary.
    However, we require this for some of our features.
    
    Change-Id: I7e4691f0cd3a569535b541d89baeb7e662004668

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -576,6 +576,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         }
     }
 
+    private Handler mMainHandler;
     private final DelayableExecutor mMainExecutor;
 
     private int mInteractingWindows;
@@ -767,6 +768,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             LockscreenShadeTransitionController lockscreenShadeTransitionController,
             FeatureFlags featureFlags,
             KeyguardUnlockAnimationController keyguardUnlockAnimationController,
+            @Main Handler mainHandler,
             @Main DelayableExecutor delayableExecutor,
             @Main MessageRouter messageRouter,
             WallpaperManager wallpaperManager,
@@ -871,6 +873,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mFeatureFlags = featureFlags;
         mIsShortcutListSearchEnabled = featureFlags.isEnabled(Flags.SHORTCUT_LIST_SEARCH_LAYOUT);
         mKeyguardUnlockAnimationController = keyguardUnlockAnimationController;
+        mMainHandler = mainHandler;
         mMainExecutor = delayableExecutor;
         mMessageRouter = messageRouter;
         mWallpaperManager = wallpaperManager;
