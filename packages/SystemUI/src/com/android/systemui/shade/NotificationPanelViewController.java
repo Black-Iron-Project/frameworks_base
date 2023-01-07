98eefe8fb38821f2acca3aa491ebd709c518da56
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-07 21:52:17 +0000

    SystemUI: Hide notifications when quickly expanding QS
    Expand/collapse animation with the 2 finger swipe/swipe down from
    right side to expand QS gesture [1] was pretty ugly with all the
    notifications being dragged along. Let's hide the notifications
    stack entirely when quickly expanding QS, for a nicer looking
    animation similar to the one in lockscreen.
    
    [1]: https://github.com/pa-gr/android_frameworks_base/commit/d3622399728c819c45c5435ddd0b1e359207f962
    
    Change-Id: Id2dbc707f9da543d47d76ae5525e827b400a4e7b

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2715,6 +2715,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             }
             mNotificationStackScrollLayoutController.setMaxAlphaForExpansion(alpha);
         }
+        if (mQsController.isExpandImmediate() && !mQsController.getFullyExpanded()) {
+            alpha = 0f;
+        }
     }
 
     private float getFadeoutAlpha() {
