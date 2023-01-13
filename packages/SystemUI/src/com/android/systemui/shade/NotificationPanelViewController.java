64f28d5577c6a4f579bff53786128503f44d7d20
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-13 14:22:37 +0200

    SystemUI: Don't force small clock when media isn't allowed on keyguard
    test: hide the media player from lockscreen via settings, have the double-line clock enabled,
    have all notifications dismissed - observe large clock still showing

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1736,7 +1736,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private int computeDesiredClockSizeForSplitShade() {
         // Media is not visible to the user on AOD.
         boolean isMediaVisibleToUser =
-                mMediaDataManager.hasActiveMediaOrRecommendation() && !isOnAod();
+                mMediaDataManager.hasActiveMediaOrRecommendation() && !isOnAod()
+                && mMediaHierarchyManager.getShouldShowOnLockScreen();
         if (isMediaVisibleToUser) {
             // When media is visible, it overlaps with the large clock. Use small clock instead.
             return SMALL;
@@ -1825,14 +1826,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     private boolean hasVisibleNotifications() {
-        if (FooterViewRefactor.isEnabled()) {
-            return mActiveNotificationsInteractor.getAreAnyNotificationsPresentValue()
-                    || mMediaDataManager.hasActiveMediaOrRecommendation();
-        } else {
+        final boolean mediaVisible = mMediaDataManager.hasActiveMediaOrRecommendation()
+                && mMediaHierarchyManager.getShouldShowOnLockScreen();
             return mNotificationStackScrollLayoutController
-                    .getVisibleNotificationCount() != 0
-                    || mMediaDataManager.hasActiveMediaOrRecommendation();
-        }
+                .getVisibleNotificationCount() != 0 || mediaVisible;
     }
 
     /** Returns space between top of lock icon and bottom of NotificationStackScrollLayout. */
