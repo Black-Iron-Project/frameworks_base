d290fdb81d80589fe0a80e9cf9d6b0183de14142
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-04 15:38:07 +0200

    allow privileged installers to maintain a global list of busy packages
    Adds a global data structure that is accessible by privileged installers and allows them to avoid
    installing the same package at the same time.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6542,6 +6542,14 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             }
             return pi;
         }
+
+        private final PrivilegedInstallerHelper privInstallerHelper =
+                new PrivilegedInstallerHelper(PackageManagerService.this);
+
+        @Override
+        public boolean updateListOfBusyPackages(boolean add, List<String> packageNames, IBinder callerBinder) {
+            return privInstallerHelper.updateListOfBusyPackages(add, packageNames, callerBinder);
+        }
     }
 
     private class PackageManagerInternalImpl extends PackageManagerInternalBase {
