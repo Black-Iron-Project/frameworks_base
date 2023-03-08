51d1c7e6bd4b5a82531ddad4014bb0d580c8913c
Matt Filetto <matt.filetto@gmail.com>
2023-03-08 19:00:21 +0200

    DozeParameters: Add content observer for pickup gesture ambient and update raise to wake gesture accordingly
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -482,6 +482,8 @@ public class DozeParameters implements
                 Settings.Secure.getUriFor(Settings.Secure.DOZE_PICK_UP_GESTURE);
         private final Uri mAlwaysOnEnabled =
                 Settings.Secure.getUriFor(Settings.Secure.DOZE_ALWAYS_ON);
+        private final Uri mPickupGestureAmbient =
+                Settings.Secure.getUriFor(Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT);
         private final Context mContext;
 
         SettingsObserver(Context context, Handler handler) {
@@ -495,6 +497,7 @@ public class DozeParameters implements
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mPickupGesture, false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(mAlwaysOnEnabled, false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(mPickupGestureAmbient, false, this, UserHandle.USER_ALL);
             update(null);
         }
 
@@ -511,6 +514,10 @@ public class DozeParameters implements
                 // the quick pickup gesture is dependent on alwaysOn being disabled and
                 // the pickup gesture being enabled
                 updateQuickPickupEnabled();
+            } else if (mPickupGestureAmbient.equals(uri)) {
+                int pickupGestureAmbient = Settings.Secure.getInt(mContext.getContentResolver(), Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT, 0);
+                int raiseToWakeGesture = pickupGestureAmbient == 1 ? 0 : 1;
+                Settings.Secure.putInt(mContext.getContentResolver(), Settings.Secure.RAISE_TO_WAKE_GESTURE, raiseToWakeGesture);
             }
         }
     }
