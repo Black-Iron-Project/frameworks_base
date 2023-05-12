1b7b0f104dbed02b34fd022d5a82b829760c5987
Dil3mm4 <dil3mm4.dev@gmail.com>
2023-05-12 09:57:01 +0000

    Feature: reticker [1/2] [someone5678] * Based on crDroid Android's cleaned A12 commit[1] * Forward port to Android 13 * Follow the Material You styles
    Original commit:
    Descendant-XI/frameworks_base@6ca0cff
    
    Ref:
    [1] crdroidandroid/android_frameworks_base@e44ede7
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6174,6 +6174,18 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * reTicker Status
+         * @hide
+         */
+        public static final String RETICKER_STATUS = "reticker_status";
+
+        /**
+         * reTicker Colored
+         * @hide
+         */
+        public static final String RETICKER_COLORED = "reticker_colored";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
