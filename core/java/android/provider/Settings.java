202f9e22576463f10ca5fc4beaf35ca85af863ae
SamarV-121 <samarvispute121@pm.me>
2023-05-29 07:29:01 +0000

    SystemUI: Add brightness slider styles [1/3] Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c Signed-off-by: rayavanindra <rayavanindra@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12200,6 +12200,12 @@ public final class Settings {
          */
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
+	/**
+         * Brightness slider styles
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
