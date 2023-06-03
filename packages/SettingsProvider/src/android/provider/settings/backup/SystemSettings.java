a9445c5fdd1eb75f9ce9be960892e5d118b89659
jhonboy121 <alfredmathew05@gmail.com>
2023-06-03 07:59:34 +0000

    SystemUI: add edge light customizations [1/2] * Drawables and layout is from omni's commit * This a completely rewritten (in kt) and a non-hacky version of their work
    * (@Stallix) - Adapt for A13 QPR1
    
    Change-Id: I4731e3c721820fbb47ebbb57d37892a3c8aa80af
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,6 +90,12 @@ public class SystemSettings {
         Settings.System.QS_TILE_LABEL_HIDE,
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
         Settings.System.QS_LAYOUT,
-        Settings.System.QQS_LAYOUT
+        Settings.System.QQS_LAYOUT,
+        Settings.System.ENABLE_RIPPLE_EFFECT,
+        Settings.System.EDGE_LIGHT_ENABLED,
+        Settings.System.EDGE_LIGHT_ALWAYS_TRIGGER_ON_PULSE,
+        Settings.System.EDGE_LIGHT_REPEAT_ANIMATION,
+        Settings.System.EDGE_LIGHT_COLOR_MODE,
+        Settings.System.EDGE_LIGHT_CUSTOM_COLOR,
     };
 }
