79fec12a78ef5e447a97cc11da9fec58ec89859b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-22 23:11:13 +0530

    SystemUI: Add FingerprintInteractiveToAuthProvider implementation
    This is required for the "touch to unlock anytime" setting on devices
    with side mounted fingerprint sensor.
    
    Ref: 66a048d646fd816e7757e300c62fa5cebf04761d
    
    Change-Id: If3308860a428d9966f2b0a0024764df95f8f9a8b

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -16,15 +16,8 @@
 
 package com.android.systemui.biometrics.dagger
 
-import android.content.res.Resources
-import com.android.internal.R
-import com.android.systemui.CoreStartable
-import com.android.systemui.biometrics.EllipseOverlapDetectorParams
-import com.android.systemui.biometrics.UdfpsUtils
-import com.android.systemui.biometrics.data.repository.BiometricStatusRepository
-import com.android.systemui.biometrics.data.repository.BiometricStatusRepositoryImpl
-import com.android.systemui.biometrics.data.repository.DisplayStateRepository
-import com.android.systemui.biometrics.data.repository.DisplayStateRepositoryImpl
+import android.content.Context
+
 import com.android.systemui.biometrics.data.repository.FacePropertyRepository
 import com.android.systemui.biometrics.data.repository.FacePropertyRepositoryImpl
 import com.android.systemui.biometrics.data.repository.FaceSettingsRepository
@@ -38,6 +31,8 @@ import com.android.systemui.biometrics.udfps.EllipseOverlapDetector
 import com.android.systemui.biometrics.udfps.OverlapDetector
 import com.android.systemui.biometrics.ui.binder.SideFpsOverlayViewBinder
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.util.concurrency.ThreadFactory
 import dagger.Binds
 import dagger.Module
@@ -114,6 +109,10 @@ interface BiometricsModule {
                 BoundingBoxOverlapDetector(values[2])
             }
         }
+
+        @Provides
+        fun providesFingerprintInteractiveToAuth(ctx: Context): FingerprintInteractiveToAuthProvider =
+            FingerprintInteractiveToAuthProviderImpl(ctx);
     }
 }
 
