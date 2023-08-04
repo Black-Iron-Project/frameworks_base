10e16c1f34f657a4509299bce8f8094097dfc20e
someone5678 <nemui3353@gmail.com>
2023-08-05 00:53:21 +0900

    SystemUI: ScrimState: Make some components transparent
    Change-Id: Ie406f26ffb2da05dc8a40db7aeec50c39d442eb1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -102,7 +102,7 @@ public enum ScrimState {
                 mBehindAlpha = mClipQsScrim ? 1 : mScrimBehindAlphaKeyguard;
                 mNotifAlpha = mClipQsScrim ? mScrimBehindAlphaKeyguard : 0;
                 if (mClipQsScrim) {
-                    updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                    updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
                 }
             }
 
@@ -123,7 +123,7 @@ public enum ScrimState {
                 return;
             }
             mBehindAlpha = mClipQsScrim ? 1 : mDefaultScrimAlpha;
-            mBehindTint = mClipQsScrim ? mBackgroundColor : mSurfaceColor;
+            mBehindTint = Color.TRANSPARENT;
             mNotifAlpha = mClipQsScrim ? mDefaultScrimAlpha : 0;
             mNotifTint = Color.TRANSPARENT;
             mFrontAlpha = 0f;
@@ -211,7 +211,7 @@ public enum ScrimState {
                 mBehindTint = mClipQsScrim ? Color.TRANSPARENT : mBackgroundColor;
 
                 if (mClipQsScrim) {
-                    updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                    updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
                 }
             }
         }
@@ -331,7 +331,7 @@ public enum ScrimState {
             }
 
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT);
             }
         }
     },
