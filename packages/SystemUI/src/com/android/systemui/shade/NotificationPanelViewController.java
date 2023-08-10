a6db4fb544273933ac012ed24032863ac983e99b
jhenrique09 <jhsv09@gmail.com>
2023-08-10 22:09:21 +0000

    Merge tag 'android-13.0.0_r67' of https://android.googlesource.com/platform/frameworks/base into thunder
    Android 13.0.0 Release 67 (TQ3A.230805.001)
    
    Change-Id: Ibf0587d48f43056adfc4784df430f6cd3acdeced

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2936,7 +2936,6 @@ public final class NotificationPanelViewController implements Dumpable {
     /** Set whether the bouncer is showing. */
     public void setBouncerShowing(boolean bouncerShowing) {
         mBouncerShowing = bouncerShowing;
-        mNotificationStackScrollLayoutController.updateShowEmptyShadeView();
         updateVisibility();
     }
 
