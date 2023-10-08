dd483da769ad965cb340a38bb417cba66bbc7614
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-08 15:41:45 +0800

    services/PPU: Simplify and improve bypassing for PPU `onTaskStackChanged` listening
    * this is for broader bypassing of tasklistener used for setCertifiedPropsForGms across apps and gms processes
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -286,8 +286,6 @@ import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 import com.android.internal.util.blackiron.cutout.CutoutFullscreenController;
 
-import com.android.internal.util.blackiron.PixelPropsUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1988,9 +1986,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("getFocusedRootTaskInfo()");
-        }
+        enforceTaskPermission("getFocusedRootTaskInfo()");
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3109,9 +3105,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("registerTaskStackListener()");
-        }
+        enforceTaskPermission("registerTaskStackListener()");
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
@@ -3310,6 +3304,18 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     }
 
     static void enforceTaskPermission(String func) {
+        IPackageManager pm = AppGlobals.getPackageManager();
+        try {
+            String[] packageNames = pm.getPackagesForUid(Binder.getCallingUid());
+            if (packageNames != null) {
+                for (String packageName : packageNames) {
+                    if (packageName.toLowerCase().contains("google")) {
+                        return;
+                    }
+                }
+            }
+        } catch (RemoteException e) {}
+
         if (checkCallingPermission(MANAGE_ACTIVITY_TASKS) == PackageManager.PERMISSION_GRANTED) {
             return;
         }
