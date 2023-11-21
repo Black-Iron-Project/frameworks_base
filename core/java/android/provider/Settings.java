bdeb9e2dd9779a33ce025457370b5399cc9f68e5
ralf979 <ralf979@gmail.com>
2023-11-21 14:16:47 +0100

    Revert "SystemUI: Bring back good ol' circle battery style"
    This reverts commit 84f347bde6df8e4a5c88a0cf05d15b87e0eb8004.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6003,26 +6003,6 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
-         * Display style of the status bar battery information
-         * 0: Display the battery an icon in portrait mode
-         * 1: Display the battery as a circle
-         * 2: Display the battery as plain text
-         * default: 0
-         * @hide
-         */
-        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-
-        /**
-         * Status bar battery %
-         * 0: Hide the battery percentage
-         * 1: Display the battery percentage inside the icon
-         * 2: Display the battery percentage next to the icon
-         * @hide
-         */
-        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
-                "status_bar_show_battery_percent";
-
-        /**
          * These are all public system settings
          *
          * @hide
@@ -6145,8 +6125,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(INCREASING_RING);
             PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
             PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
-            PRIVATE_SETTINGS.add(STATUS_BAR_BATTERY_STYLE);
-            PRIVATE_SETTINGS.add(STATUS_BAR_SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(CLICK_PARTIAL_SCREENSHOT);
         }
 
