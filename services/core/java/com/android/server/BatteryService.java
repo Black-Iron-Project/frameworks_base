47d55e4720cf83c6dcc1bef43872ee52071bf611
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-11-27 01:26:00 +0700

    Revert "BatteryService: Enhance detection of BATTERY_PLUGGED for motomods"
    This reverts commit 5dda781b582168695103d783db90528c8cd0d60b.
    
    Revert "BatteryService: Use maybeTranslatePlugType for EXTRA_PLUGGED type"
    
    This reverts commit fe18240d02dd7a2373299500268e3096043dd4b4.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -565,7 +565,7 @@ public final class BatteryService extends SystemService {
             mBatteryStats.setBatteryState(
                     mHealthInfo.batteryStatus,
                     mHealthInfo.batteryHealth,
-                    maybeTranslatePlugType(mPlugType),
+                    mPlugType,
                     mHealthInfo.batteryLevel,
                     mHealthInfo.batteryTemperatureTenthsCelsius,
                     mHealthInfo.batteryVoltageMillivolts,
@@ -597,7 +597,6 @@ public final class BatteryService extends SystemService {
                         || mBatteryModProps.modStatus != mLastModStatus
                         || mBatteryModProps.modFlag != mLastModFlag
                         || mBatteryModProps.modType != mLastModType
-                        || mBatteryModProps.modPowerSource != mLastModPowerSource
                         || mHealthInfo.chargingState != mLastCharingState
                         || mOemFastCharging != mLastOemFastCharging)) {
 
@@ -808,7 +807,7 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_BATTERY_LOW, mSentLowBatteryBroadcast);
         intent.putExtra(BatteryManager.EXTRA_SCALE, BATTERY_SCALE);
         intent.putExtra(BatteryManager.EXTRA_ICON_SMALL, icon);
-        intent.putExtra(BatteryManager.EXTRA_PLUGGED, maybeTranslatePlugType(mPlugType));
+        intent.putExtra(BatteryManager.EXTRA_PLUGGED, mPlugType);
         intent.putExtra(BatteryManager.EXTRA_VOLTAGE, mHealthInfo.batteryVoltageMillivolts);
         intent.putExtra(
                 BatteryManager.EXTRA_TEMPERATURE, mHealthInfo.batteryTemperatureTenthsCelsius);
@@ -1008,9 +1007,9 @@ public final class BatteryService extends SystemService {
             return plugType;
         }
         if (this.mHealthInfo.batteryStatus == BatteryManager.BATTERY_STATUS_CHARGING) {
-            return BatteryManager.BATTERY_PLUGGED_AC;
+            return 1;
         }
-        return BATTERY_PLUGGED_NONE;
+        return 0;
     }
 
     private boolean supplementalOrEmergencyModOnline() {
