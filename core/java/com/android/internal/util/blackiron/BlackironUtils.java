b8f6873683608f2388dce16123178f8a06c49606
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2023-11-30 07:13:21 +0100

    base: Add our Utils helper class
    From time to time we need to filter out certain features because not every
    device is created equal. This makes it easy to call from one place instead
    of adding the same code time and time again.
    
    As of right now this class includes checks for
    
    - Fingerprint support
    - Fingerprint support & enrollment
    - NFC
    - WiFi
    - Camera
    - Bluetooth
    - Mobile connection
    - Alterative ambient display (OnePlus devices)
    - A/B device support
    
    Change-Id: Ia27dc8d49a78c843f2a185f3aef195f17cc090aa
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- core/java/com/android/internal/util/blackiron/BlackironUtils.java
+++ core/java/com/android/internal/util/blackiron/BlackironUtils.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2022 FlamingoOS Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,31 +16,43 @@
 
 package com.android.internal.util.blackiron;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.hardware.fingerprint.FingerprintManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.SystemProperties;
 
 import android.os.RemoteException;
 import android.os.ServiceManager;
 
 import com.android.internal.statusbar.IStatusBarService;
 
+import com.android.internal.R;
+
 /**
  * Some custom utilities
  * @hide
  */
 public class BlackironUtils {
+
+    // Check to see if a package is installed
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
-                final PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
-                return ignoreState || pi.applicationInfo.enabled;
-            } catch (PackageManager.NameNotFoundException e) {
-                // Do nothing
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
             }
         }
-        return false;
+
+        return true;
     }
 
     public static boolean isPackageInstalled(Context context, String pkg) {
@@ -94,5 +105,56 @@ public class BlackironUtils {
         } catch (NameNotFoundException e) {
             return false;
         }
+    }
+
+    // Check to see if device is WiFi only
+    public static boolean isWifiOnly(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
+    }
+
+    // Check to see if device supports the Fingerprint scanner
+    public static boolean hasFingerprintSupport(Context context) {
+        FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
+        return context.getApplicationContext().checkSelfPermission(Manifest.permission.USE_FINGERPRINT) == PackageManager.PERMISSION_GRANTED &&
+                (fingerprintManager != null && fingerprintManager.isHardwareDetected());
+    }
+
+    // Check to see if device not only supports the Fingerprint scanner but also if is enrolled
+    public static boolean hasFingerprintEnrolled(Context context) {
+        FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
+        return context.getApplicationContext().checkSelfPermission(Manifest.permission.USE_FINGERPRINT) == PackageManager.PERMISSION_GRANTED &&
+                (fingerprintManager != null && fingerprintManager.isHardwareDetected() && fingerprintManager.hasEnrolledFingerprints());
+    }
+
+    // Check to see if device has a camera
+    public static boolean hasCamera(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    // Check to see if device supports NFC
+    public static boolean hasNFC(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC);
+    }
+
+    // Check to see if device supports Wifi
+    public static boolean hasWiFi(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI);
+    }
+
+    // Check to see if device supports Bluetooth
+    public static boolean hasBluetooth(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+    }
+
+    // Check to see if device supports an alterative ambient display package
+    public static boolean hasAltAmbientDisplay(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display);
+    }
+
+    // Check to see if device supports A/B (seamless) system updates
+    public static boolean isABdevice(Context context) {
+        return SystemProperties.getBoolean("ro.build.ab_update", false);
     }
 }
