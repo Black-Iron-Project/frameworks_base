ac85e758373a1cf5f50fe72277369e622990a8aa
Matt Mower <mowerm@gmail.com>
2023-12-01 14:04:55 +0100

    Keyguard: Forward port lockscreen quick unlock (1/2) commit 142719299d1bea5db21d17b9cc7687efe553a1a3 Author: Veeti Paananen <veeti.paananen@rojekti.fi> Date:   Tue Jun 9 12:44:59 2015 +0300
        Disable PIN entry after successful quick unlock
    
        Small polish: I still often press the enter button out of muscle memory
        when entering my PIN code, which leads to an "invalid PIN" message being
        shown for a tiny moment as the screen unlocks. Disable the entry view
        after a quick unlock to fix this.
    
        Change-Id: Icceb8311ed55564e9b1d92934cbca814a1cc4b29
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 83278c5e9fde3d1dd6a4107d2483f5cd8fc450fa
    Author: Matt Mower <mowerm@gmail.com>
    Date:   Wed Dec 31 15:02:58 2014 -0600
    
        Keyguard: Forward port lockscreen quick unlock (1/2)
    
        * PasswordTextView QuickUnlock listener functions and method thanks to
          ParanoidAndroid project:
            Keyguard: Implement quick unlock feature [1/2]
            BigBrother1984 <carlosavignano@aospa.co>
            I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
        mydongistiny: ported to oreo/android10
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Also squashed:
    Keyguard: Fix quick unlock not applying without reboot [neobuddy89]
    Make Quick Unlock compatible with long PIN/Password [Hikari-no-Tenshi]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6726,6 +6726,13 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION = "status_bar_clock_auto_hide_sduration";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL =
+                "lockscreen_quick_unlock_control";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
