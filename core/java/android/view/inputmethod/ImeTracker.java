1a8ac9f601ce4afdbc0450fb0014592e6a19079a
Dmitrii <bankersenator@gmail.com>
2023-12-05 14:48:41 +0000

    fwb: silence more spam
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/view/inputmethod/ImeTracker.java
+++ core/java/android/view/inputmethod/ImeTracker.java
@@ -446,8 +446,8 @@ public interface ImeTracker {
             final var token = IInputMethodManagerGlobalInvoker.onRequestShow(tag, uid, origin,
                     reason);
 
-            Log.i(TAG, token.mTag + ": onRequestShow at " + Debug.originToString(origin)
-                    + " reason " + InputMethodDebug.softInputDisplayReasonToString(reason));
+            //Log.i(TAG, token.mTag + ": onRequestShow at " + Debug.originToString(origin)
+            //        + " reason " + InputMethodDebug.softInputDisplayReasonToString(reason));
 
             return token;
         }
@@ -460,8 +460,8 @@ public interface ImeTracker {
             final var token = IInputMethodManagerGlobalInvoker.onRequestHide(tag, uid, origin,
                     reason);
 
-            Log.i(TAG, token.mTag + ": onRequestHide at " + Debug.originToString(origin)
-                    + " reason " + InputMethodDebug.softInputDisplayReasonToString(reason));
+            //Log.i(TAG, token.mTag + ": onRequestHide at " + Debug.originToString(origin)
+            //        + " reason " + InputMethodDebug.softInputDisplayReasonToString(reason));
 
             return token;
         }
@@ -495,7 +495,7 @@ public interface ImeTracker {
             if (token == null) return;
             IInputMethodManagerGlobalInvoker.onCancelled(token, phase);
 
-            Log.i(TAG, token.mTag + ": onCancelled at " + Debug.phaseToString(phase));
+            //Log.i(TAG, token.mTag + ": onCancelled at " + Debug.phaseToString(phase));
         }
 
         @Override
