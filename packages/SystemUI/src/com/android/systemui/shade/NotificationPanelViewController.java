a6b61a1de9b19630193743852838910b57383e4b
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 20:39:23 +0900

    Integrate Pixel framework hooks and wrappers
    [someone5678]
    * Add collapseShade, animateCollapseShade, and animateExpandNotificationsPanel
      for Quick Tap gesture
    
    Change-Id: I6f4203391d544205ea77107303fe2a5b1468e3c7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -400,8 +400,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean mTracking;
     private boolean mIsTrackingExpansionFromStatusBar;
     private boolean mHintAnimationRunning;
+    // Need public mKeyguardBottomArea for use by elmyra
     @Deprecated
-    private KeyguardBottomAreaView mKeyguardBottomArea;
+    public KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
@@ -1568,6 +1569,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
+    }
+
+    public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
+        updateMaxDisplayedNotifications(recompute);
     }
 
     private boolean shouldAvoidChangingNotificationsCount() {
@@ -3260,6 +3265,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
+    }
+
+    public void setAmbientIndicationBottomPadding(int bottomPadding) {
+        mAmbientIndicationBottomPadding = bottomPadding;
+    }
+
+    public int getAmbientIndicationBottomPadding() {
+        return mAmbientIndicationBottomPadding;
     }
 
     @Override
@@ -5487,6 +5500,16 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             }
             return super.performAccessibilityAction(host, action, args);
         }
+    }
+
+    @Override
+    public NotificationStackScrollLayoutController getScrollerLayoutController() {
+        return mNotificationStackScrollLayoutController;
+    }
+
+    @Override
+    public KeyguardBottomAreaView getKeyguardBottomAreaView() {
+        return mKeyguardBottomArea;
     }
     
     /* reTicker */
