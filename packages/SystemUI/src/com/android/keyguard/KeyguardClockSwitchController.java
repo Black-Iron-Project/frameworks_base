macka-risingOS/android_frameworks_base

Lockscreen Clock: Fix custom clock time updates
committer: rmp

approved by: hydrangea

test: manual

Change-Id: I5cbfb713fca9affcd661bb10fc58f9966740e3c3
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -71,6 +71,8 @@ import com.android.systemui.util.ViewController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.settings.SecureSettings;
 
+import com.android.systemui.afterlife.ClockStyle;
+
 import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -545,6 +547,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         if (clock != null) {
             clock.getSmallClock().getEvents().onTimeTick();
             clock.getLargeClock().getEvents().onTimeTick();
+        }
+        if (mCustomClockFrame != null) {
+        	((ClockStyle) mCustomClockFrame).onTimeChanged();
         }
     }
 

Black-Iron-Project/frameworks_base

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -83,6 +83,8 @@ import com.android.systemui.util.ViewController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.settings.SecureSettings;
 
+import com.android.systemui.blackiron.ClockStyle;
+
 import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.concurrent.Executor;
@@ -616,6 +618,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         if (clock != null) {
             clock.getSmallClock().getEvents().onTimeTick();
             clock.getLargeClock().getEvents().onTimeTick();
+        }
+        if (mCustomClockFrame != null) {
+        	((ClockStyle) mCustomClockFrame).onTimeChanged();
         }
     }
 