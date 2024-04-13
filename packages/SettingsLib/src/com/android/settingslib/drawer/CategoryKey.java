60e769c3930b2ff990f19e907d354bffcf296323
Pranav Vashi <neobuddy89@gmail.com>
2024-04-13 23:53:08 +0200

    blackiron: Adapt settings for A14
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -74,6 +74,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.more_security_privacy_settings";
     public static final String CATEGORY_OTHER =
             "com.android.settings.category.ia.other";
+    public static final String CATEGORY_BLACKIRON =
+            "com.android.settings.category.ia.blackiron";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
