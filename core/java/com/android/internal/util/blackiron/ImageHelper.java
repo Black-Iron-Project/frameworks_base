b92d0a850fca0e058bfeb8c2d0c61259f3a269b9
ralf979 <ralf979@gmail.com>
2024-04-15 07:49:09 +0200

    update copyright

--- core/java/com/android/internal/util/blackiron/ImageHelper.java
+++ core/java/com/android/internal/util/blackiron/ImageHelper.java
@@ -2,7 +2,7 @@
 * Copyright (C) 2013 SlimRoms Project
 * Copyright (C) 2015 TeamEos Project
 * Copyright (C) 2015-2016 The DirtyUnicorns Project
-* Copyright (C) 2019-2024 crDroid Android Project
+* Copyright (C) 2019-2024 BlackIron Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
