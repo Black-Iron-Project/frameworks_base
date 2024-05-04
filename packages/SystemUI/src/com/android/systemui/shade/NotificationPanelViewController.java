c7230e010620704680a4087090ac3d8f73ca0c41
Ido Ben-Hur <idoybh2@gmail.com>
2024-05-05 00:03:24 +0300

    SystemUI: Default to true for HeadsUp notifications
    WTF google??? this was always true by default. you have a config set to 1 as well.
    Please stop smoking.
    Consistency was killed with this change introduced in QPR2:
    https://android.googlesource.com/platform/frameworks/base/+/14a03f2418f4c2c74a9346f435094358b41af308
    I expect it to be fixed by google down the road.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3865,7 +3865,7 @@ public final class NotificationPanelViewController implements
                     mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
                     break;
                 case HEADS_UP_NOTIFICATIONS_ENABLED:
-                    mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, false);
+                    mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, true);
                     break;
                 default:
                     break;
