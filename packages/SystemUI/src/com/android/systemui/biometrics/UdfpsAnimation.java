a5d0db50402300954be8ba115063fe9d350d8e87
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-22 08:24:18 +0800

    SystemUI: Fix Udfps animation positioning logic
    * current implementation doesnt seem to work well on high resolution devices like cheetah 1080x3120 (full resolution).
      Usually migrating to AuthController for the positioning logic fixes the issue however for some reason when we switch
      back to high resolution (1080x2340) the udfps animation positioning is broken, to fix the inconsistencies and the positioning logic,
      use AuthController for full resolution and fallback to fingerprint props when device is using high resolution.
    * properly re-initialized the udfps animation on resolution change (onThemeChanged) and other possible configuration change
    * turn off debugging since most issues have been fixed
    
    test: Switch from high-full resolution on cheetah and vice versa. UDPFS animation location is accurate
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
@@ -24,6 +24,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
@@ -42,18 +43,18 @@ import android.graphics.Rect;
 
 import com.android.systemui.Dependency;
 import com.android.systemui.res.R;
+import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 public class UdfpsAnimation extends ImageView {
 
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private static final String LOG_TAG = "UdfpsAnimations";
 
     private boolean mShowing = false;
     private Context mContext;
     private int mAnimationSize;
-    private int mAnimationOffset;
     private AnimationDrawable recognizingAnim;
 
     private final WindowManager.LayoutParams mAnimParams = new WindowManager.LayoutParams();
@@ -71,17 +72,21 @@ public class UdfpsAnimation extends ImageView {
     private Resources mApkResources;
     
     private final KeyguardStateController mKeyguardStateController;
+    private final AuthController mAuthController;
+    private final FingerprintSensorPropertiesInternal mProps;
 
     public UdfpsAnimation(Context context, WindowManager windowManager,
-           FingerprintSensorPropertiesInternal props) {
+           FingerprintSensorPropertiesInternal props, AuthController authController) {
         super(context);
         mContext = context;
+        mAuthController = authController;
+        mProps = props;
 
         mWindowManager = windowManager;
         
         mKeyguardStateController = Dependency.get(KeyguardStateController.class);
 
-        final float scaleFactor = DisplayUtils.getScaleFactor(mContext);
+        float scaleFactor = getDisplayFactor();
 
         mMaxBurnInOffsetX = (int) (context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x) * scaleFactor);
@@ -89,7 +94,6 @@ public class UdfpsAnimation extends ImageView {
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y) * scaleFactor);
 
         mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_size);
-        mAnimationOffset = (int) (mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_offset) * scaleFactor);
 
         mAnimParams.height = mAnimationSize;
         mAnimParams.width = mAnimationSize;
@@ -100,8 +104,8 @@ public class UdfpsAnimation extends ImageView {
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
                 | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
         mAnimParams.gravity = Gravity.TOP | Gravity.CENTER;
-        mAnimParams.y = (int) (props.getLocation().sensorLocationY * scaleFactor) - (int) (props.getLocation().sensorRadius * scaleFactor)
-                - (mAnimationSize / 2) + mAnimationOffset;
+
+        updatePosition();
 
         try {
             PackageManager pm = mContext.getPackageManager();
@@ -169,6 +173,33 @@ public class UdfpsAnimation extends ImageView {
     public boolean isAnimationEnabled() {
         return recognizingAnim != null;
     }
+    
+    private float getDisplayFactor() {
+        return DisplayUtils.getScaleFactor(mContext);
+    }
+    
+    public void updatePosition() {
+        Point displaySize = new Point();
+        mWindowManager.getDefaultDisplay().getRealSize(displaySize);
+        boolean isFullResolution = displaySize.y > 3000; 
+        Point udfpsLocation = mAuthController.getUdfpsLocation();
+        float scaleFactor = getDisplayFactor();
+        float udfpsRadius = isFullResolution ? mAuthController.getUdfpsRadius() : mProps.getLocation().sensorRadius;
+        float udfpsLocationY = isFullResolution && udfpsLocation != null ? udfpsLocation.y : mProps.getLocation().sensorLocationY;
+        int animationOffset = (int) (mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_offset) * scaleFactor);
+        mAnimParams.y = (int) (udfpsLocationY * scaleFactor) - (int) (udfpsRadius * scaleFactor)
+                        - (mAnimationSize / 2) + animationOffset;
+        if (DEBUG) {
+            Log.d(LOG_TAG, "updatePosition: displaySize=" + displaySize + 
+                           ", isFullResolution=" + isFullResolution + 
+                           ", udfpsLocation=" + udfpsLocation + 
+                           ", udfpsRadius=" + udfpsRadius + 
+                           ", scaleFactor=" + scaleFactor + 
+                           ", udfpsLocationY=" + udfpsLocationY + 
+                           ", animationOffset=" + animationOffset + 
+                           ", mAnimParams.y=" + mAnimParams.y);
+        }
+    }
 
     public void show() {
         if (!mShowing && mIsKeyguard && isAnimationEnabled()) {
@@ -194,7 +225,7 @@ public class UdfpsAnimation extends ImageView {
         }
     }
     
-    private void removeAnimation() {
+    public void removeAnimation() {
         if (recognizingAnim != null) {
             clearAnimation();
             recognizingAnim.stop();
