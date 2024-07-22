fazilsheik96/android_frameworks_base

display: Consider peak refresh rate in BrightnessObserver
If the peak refresh rate set by the user is lower than the low/high
zone refresh set in the device overlay, it switches to 60Hz. This is
not ideal, as for example, the user may have set the peak refresh rate
to 90Hz (while the overlay defines 120Hz as the refresh rate in the low
brightness zone), but at low brightness the display enters 60Hz instead
of staying at 90Hz, leading to poor UX.

Use the minimum of the user-set peak brightness and the predefined
refresh rate of the brightness zone to avoid this behaviour.

Change-Id: Ia69fd5e8221bedb05b968d1eedebb4823ad4a9b7

services/core/java/com/android/server/display/DisplayModeDirector.java
@@ -1672,6 +1672,7 @@ public class DisplayModeDirector {
 
         private int mRefreshRateInLowZone;
         private int mRefreshRateInHighZone;
+        private int mPeakRefreshRate;
 
         BrightnessObserver(Context context, Handler handler, Injector injector) {
             mContext = context;
@@ -1918,10 +1919,16 @@ public class DisplayModeDirector {
 
         public void onRefreshRateSettingChangedLocked(float min, float max) {
             boolean changeable = (max - min > 1f && max > 60f);
-            if (mRefreshRateChangeable != changeable) {
+            int peakRefreshRate = Math.round(Math.max(min, max));
+            if (mRefreshRateChangeable != changeable || mPeakRefreshRate != peakRefreshRate) {
                 mRefreshRateChangeable = changeable;
-                updateSensorStatus();
-                if (!changeable) {
+                mPeakRefreshRate = peakRefreshRate;
+                if (changeable) {
+                    synchronized (mLock) {
+                        onBrightnessChangedLocked();
+                    }
+                } else {
+                    updateSensorStatus();
                     // Revoke previous vote from BrightnessObserver
                     updateVoteLocked(Vote.PRIORITY_FLICKER_REFRESH_RATE, null);
                     updateVoteLocked(Vote.PRIORITY_FLICKER_REFRESH_RATE_SWITCH, null);
@@ -2227,18 +2234,20 @@ public class DisplayModeDirector {
                 return;
             }
 
+            int refreshRateInLowZone = Math.min(mPeakRefreshRate, mRefreshRateInLowZone);
             boolean insideLowZone = hasValidLowZone() && isInsideLowZone(mBrightness, mAmbientLux);
             if (insideLowZone) {
                 refreshRateVote =
-                        Vote.forRefreshRates(mRefreshRateInLowZone, mRefreshRateInLowZone);
+                        Vote.forRefreshRates(refreshRateInLowZone, refreshRateInLowZone);
                 refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
             }
 
+            int refreshRateInHighZone = Math.min(mPeakRefreshRate, mRefreshRateInHighZone);
             boolean insideHighZone = hasValidHighZone()
                     && isInsideHighZone(mBrightness, mAmbientLux);
             if (insideHighZone) {
                 refreshRateVote =
-                        Vote.forRefreshRates(mRefreshRateInHighZone, mRefreshRateInHighZone);
+                        Vote.forRefreshRates(refreshRateInHighZone, refreshRateInHighZone);
                 refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
             }
 

Black-Iron-Project/frameworks_base

services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1568,6 +1568,7 @@ public class DisplayModeDirector {
         @Nullable
         private SparseArray<RefreshRateRange> mHighZoneRefreshRateForThermals;
         private int mRefreshRateInHighZone;
+        private int mPeakRefreshRate;
 
         @GuardedBy("mLock")
         private @Temperature.ThrottlingStatus int mThermalStatus = Temperature.THROTTLING_NONE;
@@ -1808,10 +1809,16 @@ public class DisplayModeDirector {
         @VisibleForTesting
         public void onRefreshRateSettingChangedLocked(float min, float max) {
             boolean changeable = (max - min > 1f && max > 60f);
-            if (mRefreshRateChangeable != changeable) {
+            int peakRefreshRate = Math.round(Math.max(min, max));
+            if (mRefreshRateChangeable != changeable || mPeakRefreshRate != peakRefreshRate) {
                 mRefreshRateChangeable = changeable;
-                updateSensorStatus();
-                if (!changeable) {
+                mPeakRefreshRate = peakRefreshRate;
+                if (changeable) {
+                    synchronized (mLock) {
+                        onBrightnessChangedLocked();
+                    }
+                } else {
+                    updateSensorStatus();
                     removeFlickerRefreshRateVotes();
                 }
             }
@@ -2148,10 +2155,11 @@ public class DisplayModeDirector {
                 return;
             }
 
+            int refreshRateInLowZone = Math.min(mPeakRefreshRate, mRefreshRateInLowZone);
             boolean insideLowZone = hasValidLowZone() && isInsideLowZone(mBrightness, mAmbientLux);
             if (insideLowZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInLowZone, mRefreshRateInLowZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInLowZone, refreshRateInLowZone);
                 if (mLowZoneRefreshRateForThermals != null) {
                     RefreshRateRange range = SkinThermalStatusObserver
                             .findBestMatchingRefreshRateRange(mThermalStatus,
@@ -2174,12 +2182,12 @@ public class DisplayModeDirector {
                 }
             }
 
+            int refreshRateInHighZone = Math.min(mPeakRefreshRate, mRefreshRateInHighZone);
             boolean insideHighZone = hasValidHighZone()
                     && isInsideHighZone(mBrightness, mAmbientLux);
             if (insideHighZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInHighZone,
-                                mRefreshRateInHighZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInHighZone, refreshRateInHighZone);
                 if (mHighZoneRefreshRateForThermals != null) {
                     RefreshRateRange range = SkinThermalStatusObserver
                             .findBestMatchingRefreshRateRange(mThermalStatus,