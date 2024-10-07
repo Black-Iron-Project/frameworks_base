da77cf78c038a4ae99757e97f0406fff05b6fdcd
Tommy Webb <tommy@calyxinstitute.org>
2024-10-07 19:13:04 +0000

    Do not undo Dark Theme schedule override so often
    Stop resetting the override state for Dark Theme every time a battery
    broadcast is received, when automatic (sunrise/sunset) or custom
    schedules are configured. Instead, only reset the overrides when the
    time or timezone changes, or the begin/end time is reached. This allows
    the user to turn Dark Theme off or on regardless of the schedule without
    it being undone in a matter of moments.
    
    Bug: 366644924
    Issue: calyxos#2633
    Change-Id: I477d50c74eb244r1059p41fccce0d1836cs7t415

--- services/core/java/com/android/server/UiModeManagerService.java
+++ services/core/java/com/android/server/UiModeManagerService.java
@@ -316,6 +316,7 @@ final class UiModeManagerService extends SystemService {
         public void onTwilightStateChanged(@Nullable TwilightState state) {
             synchronized (mLock) {
                 if (mNightMode.get() == UiModeManager.MODE_NIGHT_AUTO && mSystemReady) {
+                    resetNightModeOverrideLocked();
                     if (shouldApplyAutomaticChangesImmediately()) {
                         updateLocked(0, 0);
                     } else {
@@ -609,6 +610,7 @@ final class UiModeManagerService extends SystemService {
 
     private void updateCustomTimeLocked() {
         if (mNightMode.get() != MODE_NIGHT_CUSTOM) return;
+        resetNightModeOverrideLocked();
         if (shouldApplyAutomaticChangesImmediately()) {
             updateLocked(0, 0);
         } else {
@@ -2076,8 +2078,9 @@ final class UiModeManagerService extends SystemService {
             mComputedNightMode = newComputedValue;
         }
 
-        if (mNightMode.get() != MODE_NIGHT_AUTO || (mTwilightManager != null
-                && mTwilightManager.getLastTwilightState() != null)) {
+        // Reset night mode overrides when not in auto/time-based modes, which reset them elsewhere.
+        final int nightMode = mNightMode.get();
+        if (nightMode != MODE_NIGHT_AUTO && nightMode != MODE_NIGHT_CUSTOM) {
             resetNightModeOverrideLocked();
         }
     }
