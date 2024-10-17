b0c3965d0dbca51b456b6a2c8593cfc9e465980e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-17 16:53:46 +0800

    SystemUI: Introduce AOD styles [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/weather/WeatherViewController.kt
+++ packages/SystemUI/src/com/android/systemui/weather/WeatherViewController.kt
@@ -24,7 +24,7 @@ import android.provider.Settings
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import com.android.internal.util.android.OmniJawsClient
+import com.android.internal.util.blackiron.OmniJawsClient
 import com.android.systemui.res.R
 
 class WeatherViewController(
