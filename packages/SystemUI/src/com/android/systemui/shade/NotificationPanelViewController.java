91a5002974c29c2c308d7f812c671954aee135e2
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-28 18:37:00 +0800

    SystemUI: Set load up hint when expanding QuickSettings
    * To avoid jank when expanding Quick Settings, we must ensure that HWUI can handle the workload to prevent upcoming spikes,
      especially under heavy load. This can be achieved by sending hints before and after requestLayout is called
    
    test: play media to show QS media panel -> expand/collapse quick settings
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1292,11 +1292,23 @@ public final class NotificationPanelViewController implements
         return mActiveNotificationsInteractor.getAreAnyNotificationsPresentValue()
                 || mMediaDataManager.hasActiveMediaOrRecommendation();
     }
+    
+    private void boostFrames() {
+        if (mView != null && mView.getViewRootImpl() != null) {
+            mView.getViewRootImpl().notifyRendererOfExpensiveFrame();
+        }
+    }
+    
+    private void boostFramesDuringRelayout() {
+        boostFrames();
+        this.mView.requestLayout();
+        boostFrames();
+    }
 
     @Override
     public void transitionToExpandedShade(long delay) {
         mNotificationStackScrollLayoutController.goToFullShade(delay);
-        mView.requestLayout();
+        boostFramesDuringRelayout();
         mAnimateNextPositionUpdate = true;
     }
 
@@ -2138,7 +2150,7 @@ public final class NotificationPanelViewController implements
                         }
                     });
             // Make sure a layout really happens.
-            this.mView.requestLayout();
+            boostFramesDuringRelayout();
         }
 
         setListening(true);
@@ -3424,7 +3436,7 @@ public final class NotificationPanelViewController implements
         //A layout will ensure that onComputeInternalInsets will be called and after that we can
         // resize the layout. Make sure that the window stays small for one frame until the
         // touchableRegion is set.
-        mView.requestLayout();
+        boostFramesDuringRelayout();
         mNotificationShadeWindowController.setForceWindowCollapsed(true);
         postToView(() -> {
             mNotificationShadeWindowController.setForceWindowCollapsed(false);
