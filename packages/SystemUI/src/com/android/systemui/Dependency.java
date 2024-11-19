3f82bab63548cc48df4f6ae6cbcdf5c5cb110211
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-19 11:39:43 +0800

    SystemUI: Introduce Depth Wallpaper feature [1/2]
    * like ios wallpaper depth, taken from pixel xpert
    
    Co-authored-by: Siavash <siavash@gmail.com>
    Change-Id: I395856922f25dd45ec2a2e535bff4cd0903a0834
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -56,6 +56,9 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.tuner.TunerService;
 
+import com.android.systemui.qs.QSImpl;
+import com.android.systemui.statusbar.phone.ScrimController;
+
 import dagger.Lazy;
 
 import java.util.function.Consumer;
@@ -151,6 +154,8 @@ public class Dependency {
     @Inject Lazy<DialogTransitionAnimator> mDialogTransitionAnimatorLazy;
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
+    @Inject Lazy<QSImpl> mQSImpl;
+    @Inject Lazy<ScrimController> mScrimController;
 
     @Inject
     public Dependency() {
@@ -197,6 +202,8 @@ public class Dependency {
         mProviders.put(UserTracker.class, mUserTrackerLazy::get);
         mProviders.put(
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
+        mProviders.put(QSImpl.class, mQSImpl::get);
+        mProviders.put(ScrimController.class, mScrimController::get);
 
         Dependency.setInstance(this);
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -698,6 +698,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         return mState;
     }
 
+    public float getScrimBehindAlpha() {
+        return mScrimBehindAlphaKeyguard;
+    }
+
     /**
      * Sets the additional scrim behind alpha keyguard that would be blended with the default scrim
      * by applying alpha composition on both values.
@@ -1195,6 +1199,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         setOrAdaptCurrentAnimation(mScrimInFront);
         dispatchBackScrimState(mScrimBehind.getViewAlpha());
         MediaArtUtils.getInstance(mScrimBehind.getContext()).updateMediaArtVisibility();
+        com.android.systemui.util.WallpaperDepthUtils.getInstance(mScrimBehind.getContext()).updateDepthWallpaper();
     }
 
     /**
