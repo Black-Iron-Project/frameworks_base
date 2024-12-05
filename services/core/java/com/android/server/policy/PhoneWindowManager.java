657cd3b152ffc613771a3d0100f00ebafab6f068
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-05 11:50:43 +0800

    Pocket Mode: Fix long press power button behaviour
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1716,9 +1716,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case LONG_PRESS_POWER_HIDE_POCKET_LOCK:
                 mPowerKeyHandled = true;
                 if (mPocketModeService != null) {
-                    mPocketModeService.onInteractiveChanged(false);
+                    mPocketModeService.disablePocketLock();
                 }
-                performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false, "Power - Long-Press - Hide Pocket Lock");
                 break;
         }
     }
