b76d8cc447a5b96bc4c627f40545c9b40fb222a6
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-07 10:14:03 +0900

    base: Add support for hide applist [1/2]
    * Based on commit "Hide hidden apps from all apps except system"
    * Hide user selected apps from applist
    * The applist includes system apps (including overlays), and user apps
    
    Ref:
    https://review.calyxos.org/c/CalyxOS/platform_frameworks_base/+/16571
    https://github.com/TheParasiteProject/frameworks_base/commit/6c41a0fc2c7ba01fd81132ceb0f3c1476099cf25
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
    Change-Id: If6c94fd345c488edab11927c4f567bc2171f69b2
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -2624,6 +2624,11 @@ public class ComputerEngine implements Computer {
         if (ps.getUserStateOrDefault(userId).isHidden()) {
             return true;
         }
+        // if the target is included in Settings.Secure.HIDE_APPLIST, do filter
+        if (com.android.internal.util.blackiron.HideAppListUtils.shouldHideAppList(
+                mContext, packageName)) {
+            return true;
+        }
 
         return false;
     }
