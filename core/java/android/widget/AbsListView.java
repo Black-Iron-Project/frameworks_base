c278e98625e8d16b1ce6e1427e0789970db90a94
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-21 10:35:37 +0800

    notify HWUI when scrolling
    reduce janks when scrolling list views

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -3635,6 +3635,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
     }
 
     private boolean startScrollIfNeeded(int x, int y, MotionEvent vtev) {
+        notifyExpensiveFrame();
         // Check if we have moved far enough that it looks more like a
         // scroll than a tap
         final int deltaY = y - mMotionY;
@@ -4987,6 +4988,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         // Use AbsListView#fling(int) instead
         @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.P)
         void start(int initialVelocity) {
+            notifyExpensiveFrame();
             if (Math.abs(initialVelocity) > mDecacheThreshold) {
                 // For long flings, scrolling cache causes stutter, so don't use it
                 clearScrollingCache();
@@ -5015,6 +5017,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         void startSpringback() {
+            notifyExpensiveFrame();
             mSuppressIdleStateChangeCall = false;
             if (mScroller.springBack(0, mScrollY, 0, 0, 0, 0)) {
                 mTouchMode = TOUCH_MODE_OVERFLING;
@@ -5027,6 +5030,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         void startOverfling(int initialVelocity) {
+            notifyExpensiveFrame();
             mScroller.setInterpolator(null);
             mScroller.fling(0, mScrollY, 0, initialVelocity, 0, 0,
                     Integer.MIN_VALUE, Integer.MAX_VALUE, 0, getHeight());
@@ -5060,6 +5064,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         void startScroll(int distance, int duration, boolean linear,
                 boolean suppressEndFlingStateChangeCall) {
+            notifyExpensiveFrame();
             int initialY = distance < 0 ? Integer.MAX_VALUE : 0;
             mLastFlingY = initialY;
             mScroller.setInterpolator(linear ? sLinearInterpolator : null);
@@ -5235,6 +5240,12 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 break;
             }
             }
+        }
+    }
+    
+    void notifyExpensiveFrame() {
+        if (getViewRootImpl() != null) {
+            getViewRootImpl().notifyRendererOfExpensiveFrame();
         }
     }
 
