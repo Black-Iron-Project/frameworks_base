8ea99a827e1a495d372ec84c30e4a8d2c09b1108
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-04 11:17:43 +0800

    SystemUI: Do not crash SystemUI if smartspace cannot be built
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: java.lang.RuntimeException: Cannot build weather view when not decoupled
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.statusbar.lockscreen.LockscreenSmartspaceController.buildAndConnectWeatherView(LockscreenSmartspaceController.kt:363)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.view.layout.sections.SmartspaceSection.addViews(SmartspaceSection.kt:74)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.shared.model.KeyguardBlueprint.replaceViews(KeyguardBlueprint.kt:48)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.shared.model.KeyguardBlueprint.replaceViews$default(KeyguardBlueprint.kt:37)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.binder.KeyguardBlueprintViewBinder$bind$1$1$1$1$1.invoke(KeyguardBlueprintViewBinder.kt:80)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.binder.KeyguardBlueprintViewBinder$bind$1$1$1$1$1.invoke(KeyguardBlueprintViewBinder.kt:78)
    01-04 11:16:16.486  4645  4645 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.viewmodel.KeyguardBlueprintViewModel$runTransition$2.run(KeyguardBlueprintViewModel.kt:125)

--- packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceController.kt
@@ -346,11 +346,8 @@ constructor(
     fun buildAndConnectDateView(parent: ViewGroup): View? {
         execution.assertIsMainThread()
 
-        if (!isEnabled) {
-            throw RuntimeException("Cannot build view when not enabled")
-        }
-        if (!isDateWeatherDecoupled) {
-            throw RuntimeException("Cannot build date view when not decoupled")
+        if (!isEnabled || !isDateWeatherDecoupled) {
+            return null
         }
 
         val view =
@@ -370,11 +367,8 @@ constructor(
     fun buildAndConnectWeatherView(parent: ViewGroup): View? {
         execution.assertIsMainThread()
 
-        if (!isEnabled) {
-            throw RuntimeException("Cannot build view when not enabled")
-        }
-        if (!isDateWeatherDecoupled) {
-            throw RuntimeException("Cannot build weather view when not decoupled")
+        if (!isEnabled || !isDateWeatherDecoupled) {
+            return null
         }
 
         val view =
