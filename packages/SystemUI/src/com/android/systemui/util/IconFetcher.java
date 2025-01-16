be1518f07a646bd4d87a940304916912219b3f09
f104a <f104a@f104a.io>
2025-01-16 15:57:43 +0100

    SystemUI: Don't tint progressbar chip icon
    Change-Id: I5de1ff070e8ced513db5e4867728fcd727e5f511

--- packages/SystemUI/src/com/android/systemui/util/IconFetcher.java
+++ packages/SystemUI/src/com/android/systemui/util/IconFetcher.java
@@ -53,7 +53,6 @@ public class IconFetcher {
      *     AdaptiveDrawableResult
      */
     public AdaptiveDrawableResult getMonotonicPackageIcon(String packageName) {
-        int tintColor = Color.WHITE;
         try {
             PackageManager packageManager = mContext.getPackageManager();
             Drawable icon = packageManager.getApplicationIcon(packageName);
@@ -63,8 +62,6 @@ public class IconFetcher {
 
                 Drawable foreground = adaptiveIcon.getForeground();
 
-                foreground.setColorFilter(
-                        new PorterDuffColorFilter(tintColor, PorterDuff.Mode.SRC_IN));
                 return new AdaptiveDrawableResult(true, icon);
             } else {
                 return new AdaptiveDrawableResult(false, icon);
