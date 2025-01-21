9396bbf1fba4a14ab9f62bbca5bbff9804ca3e47
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-21 20:25:10 +0900

    ComputerEngine: Don't hide hidden apps
    * Since we have Hide Applist support,
      we don't need to hide it by default
    
    Change-Id: I2f49f43043e52ec0ac167e18e6fc90938f30214b

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -2620,10 +2620,6 @@ public class ComputerEngine implements Computer {
                 || isCallerHome(callingUid, userId)) {
             return false;
         }
-        // if the target is hidden app, do filter
-        if (ps.getUserStateOrDefault(userId).isHidden()) {
-            return true;
-        }
         // if the target is included in Settings.Secure.HIDE_APPLIST, do filter
         if (com.android.internal.util.blackiron.HideAppListUtils.shouldHideAppList(
                 mContext, packageName)) {
