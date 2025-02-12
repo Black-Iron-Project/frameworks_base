53e99d44f7fc8ce612f1b1e88cc523472f9b9f73
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-12 10:18:23 +0530

    SystemUI: GlobalActions: Hide user switcher in some more cases
    - When secondary user(s) cannot be switched to (such as parallel space)
    - When secondary user is present but multi-user is turned off.
    
    Change-Id: Ib2cebc3ecf5b67880339b174ac8aae4c866a2b10

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -171,6 +171,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
@@ -671,9 +672,12 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 if (mShowSilentToggle) {
                     addIfShouldShowAction(tempActions, mSilentModeAction);
                 }
-            } else if (GLOBAL_ACTION_KEY_USERS.equals(actionKey)) {
-                List<UserInfo> users = mUserManager.getUsers();
-                if (users.size() > 1) {
+            } else if (GLOBAL_ACTION_KEY_USERS.equals(actionKey)
+                    && mUserManager.isUserSwitcherEnabled()) {
+                Stream<UserInfo> users = mUserManager.getUsers()
+                        .stream()
+                        .filter(u -> u.supportsSwitchToByUser());
+                if (users.count() > 1) {
                     addUserActions(mUsersItems, currentUser.get());
                     addIfShouldShowAction(tempActions, new UsersAction());
                 }
