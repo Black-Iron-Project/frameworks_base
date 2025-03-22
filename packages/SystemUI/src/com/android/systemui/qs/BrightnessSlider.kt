36a2a0051cc233ea5933c1be1b0db4c86f640e71
Arman-ATI <altafiarman5@gmail.com>
2025-03-22 15:00:44 +0000

    Adapt QS widgets for 15-QPR2

--- packages/SystemUI/src/com/android/systemui/qs/BrightnessSlider.kt
+++ packages/SystemUI/src/com/android/systemui/qs/BrightnessSlider.kt
@@ -85,8 +85,7 @@ class BrightnessSlider(context: Context, attrs: AttributeSet? = null) :
         )
         displayManager.registerDisplayListener(
             mDisplayListener,
-            mHandler,
-            DisplayManager.EVENT_FLAG_DISPLAY_BRIGHTNESS
+            mHandler
         )
     }
 
