76c56d047851dfc053c6f7cf983f1e6050eac545
Ghosuto <clash.raja10@gmail.com>
2025-04-05 13:03:01 +0000

    SystemUI: Allow to change progressbar opacity [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6876,6 +6876,11 @@ public final class Settings {
          * @hide
          */
         public static final String SHOW_MEDIA_PROGRESS = "show_media_progress";
+        
+        /**
+         * @hide
+         */
+        public static final String PROGRESS_BAR_OPACITY = "progress_bar_opacity";
 
         /**
          * @hide
