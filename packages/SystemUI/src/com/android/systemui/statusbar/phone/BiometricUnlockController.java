0674f7c7bd9859447ad1403965ec5a824430d228
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-30 18:38:06 +0800

    [Biometrics] increase udfps retry limit
    Change-Id: I7c5fcbfba3a80ab3b971b5edb1720ae2400ad81a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -90,7 +90,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
     private static final long BIOMETRIC_WAKELOCK_TIMEOUT_MS = 15 * 1000;
     private static final String BIOMETRIC_WAKE_LOCK_NAME = "wake-and-unlock:wakelock";
     private static final UiEventLogger UI_EVENT_LOGGER = new UiEventLoggerImpl();
-    private static final int UDFPS_ATTEMPTS_BEFORE_SHOW_BOUNCER = 3;
+    private static final int UDFPS_ATTEMPTS_BEFORE_SHOW_BOUNCER = 5;
 
     @IntDef(prefix = { "MODE_" }, value = {
             MODE_NONE,
