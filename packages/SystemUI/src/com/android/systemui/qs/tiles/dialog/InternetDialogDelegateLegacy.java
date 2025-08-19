61d4111793626adebcf974fa1411195547798dbb
Arman-ATI <altafiarman5@gmail.com>
2025-08-19 06:27:18 +0000

    Fix NPE with QS internet dialog
    08-19 09:33:26.369 23282 23319 E AndroidRuntime: FATAL EXCEPTION: SysUiBg
    08-19 09:33:26.369 23282 23319 E AndroidRuntime: Process: com.android.systemui, PID: 23282
    08-19 09:33:26.369 23282 23319 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'android.graphics.drawable.Drawable android.graphics.drawable.Drawable$ConstantState.newDrawable()' on a null object reference
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at com.android.systemui.qs.tiles.dialog.InternetDialogDelegateLegacy$$ExternalSyntheticLambda1.run(go/retraceme bff7401e839ae4e59c53a9e0e4c52eac64c1165f12047b06b9a7019433ea3dab:20)
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at android.os.Handler.handleCallback(Handler.java:995)
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at android.os.Handler.dispatchMessage(Handler.java:103)
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at android.os.Looper.loopOnce(Looper.java:248)
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at android.os.Looper.loop(Looper.java:338)
    08-19 09:33:26.369 23282 23319 E AndroidRuntime:     at android.os.HandlerThread.run(HandlerThread.java:85)

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -350,7 +350,11 @@ public class InternetDialogDelegateLegacy implements
         mInternetDialogTitle.setText(getDialogTitleText());
         mInternetDialogTitle.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
         mBackgroundOff = context.getDrawable(R.drawable.internet_dialog_selected_effect);
-        mSecondaryBackgroundOn = mBackgroundOn.getConstantState().newDrawable().mutate();
+        if (mBackgroundOn != null && mBackgroundOn.getConstantState() != null) {
+            mSecondaryBackgroundOn = mBackgroundOn.getConstantState().newDrawable().mutate();
+        } else {
+            mSecondaryBackgroundOn = mBackgroundOn;
+        }
         setOnClickListener(dialog);
         setHotspotLayout();
         mTurnWifiOnLayout.setBackground(null);
