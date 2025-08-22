deb1e8e1329ff79b6b7105ee46c3d354030d5226
Pranav Vashi <neobuddy89@gmail.com>
2025-08-23 00:40:29 +0530

    fixup! SystemUI: Remove more unnecessary smartspace spaces
    * Just clean up unused/duplicate stuff.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
@@ -165,8 +165,6 @@ constructor(
         val guideline =
             if (keyguardClockViewModel.clockShouldBeCentered.value) PARENT_ID
             else R.id.split_shade_guideline
-        val dateWeatherBelowSmallClock =
-            KeyguardSmartspaceViewModel.dateWeatherBelowSmallClock(context.resources.configuration)
         constraints.apply {
             connect(customR.id.lockscreen_clock_view_large, START, PARENT_ID, START)
             connect(customR.id.lockscreen_clock_view_large, END, guideline, END)
@@ -215,7 +213,7 @@ constructor(
             setTransformPivot(customR.id.lockscreen_clock_view_large, Float.NaN, Float.NaN)
 
             val smallClockBottom =
-                keyguardClockViewModel.getSmallClockTopMargin() +
+                smallClockTopMargin +
                     context.resources.getDimensionPixelSize(customR.dimen.small_clock_height)
             val marginBetweenSmartspaceAndNotification =
                 context.resources.getDimensionPixelSize(
@@ -227,16 +225,9 @@ constructor(
                         0
                     }
 
-            if (dateWeatherBelowSmallClock) {
-                clockInteractor.setNotificationStackDefaultTop(
-                    (smallClockBottom +
-                        marginBetweenSmartspaceAndNotification).toFloat()
-                )
-            } else {
-                clockInteractor.setNotificationStackDefaultTop(
-                    (smallClockBottom + marginBetweenSmartspaceAndNotification).toFloat()
-                )
-            }
+            clockInteractor.setNotificationStackDefaultTop(
+                (smallClockBottom + marginBetweenSmartspaceAndNotification).toFloat()
+            )
         }
 
         constrainWeatherClockDateIconsBarrier(constraints)
