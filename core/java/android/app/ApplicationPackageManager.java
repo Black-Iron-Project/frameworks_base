5fe5df30414cc396b59191c6d20d6245f76a66a0
ralf979 <ralf979@gmail.com>
2025-08-22 16:02:44 +0200

    Revert "ApplicationPackageManager: Allow Tensor features to Google Dialer"
    This reverts commit 4fb789f28f9487b8e5880030ae37f05145721e0c.

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -930,8 +930,7 @@ public class ApplicationPackageManager extends PackageManager {
             if (Arrays.asList(featuresNexus).contains(name)) return true;
         }
         if (name != null && Arrays.asList(featuresTensor).contains(name)
-                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.product.device"))
-                && (packageName == null || !packageName.equals("com.google.android.dialer"))) {
+                && !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.product.device"))) {
             return false;
         }
         if (Arrays.asList(featuresPixel).contains(name)) return true;
